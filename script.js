6af16aca89522cf112aa44899aceb62b940d64e6
TibiCica <kovacskd13@gmail.com>
2024-11-29 12:49:27 +0100

    ITS GORGEOUS!
    slick content + css animations + adjustments in js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 document.getElementById("mobbinos").addEventListener("change", changepicture);
 document.getElementById("ZaWarudo").addEventListener("change", sel2);
 document.getElementById("searcher").addEventListener("keyup", sel3);
+document.getElementById("mobbinos").addEventListener("click", changepicture);
+document.getElementById("ZaWarudo").addEventListener("click", sel2);
 
 function mobsel()
 {
@@ -55,35 +57,12 @@ function addplayer()
 function changepicture() {
     document.getElementById("mobs").innerHTML = "";
     addplayer();
-    const selectElement = document.getElementById("mobbinos");
-    Array.from(selectElement.selectedOptions).forEach(option => {
-        const div = document.createElement("div");
-        div.className = "mob";
-        const img = document.createElement("img");
-        img.src = mobs[option.value].image;
-        img.alt = mobs[option.value].name;
-        img.id = "creature";
-        div.appendChild(img);
-        const h1 = document.createElement("h1");
-        h1.innerHTML=mobs[option.value].name;
-        div.appendChild(h1);
-        const ddiv = document.createElement("div");
-        ddiv.style.width="45%";
-        ddiv.style.float="right";
-        ddiv.style.margin=0;
-        ddiv.className="general";
-        ddiv.innerHTML="HP: <br>";
-        for (i=1; i<=mobs[option.value].health;i++)
-            i%2==1 ? ddiv.innerHTML+='<img src="képek/heart.png" class="heart">' :  ddiv.innerHTML+='<img src="képek/heart.png" class="heart flipped">' ;
-        if (mobs[option.value].health==0)
-            ddiv.innerHTML+=" None";
-        ddiv.innerHTML+="<br>Atk: <br>";
-        for (i=1; i<=mobs[option.value].dmg;i++)
-            i%2==1 ? ddiv.innerHTML+='<img src="képek/heart.png" class="heart">' :  ddiv.innerHTML+='<img src="képek/heart.png" class="heart flipped">' ;
-        if (mobs[option.value].dmg==0)
-            ddiv.innerHTML+=" None";
-        div.appendChild(ddiv);
-        document.getElementById("mobs").appendChild(div);
+    const SelElem = document.getElementById("mobbinos");
+    mobs.forEach(mob => {
+        Array.from(SelElem.selectedOptions).forEach(option => {
+            if (option.innerHTML==mob.name)
+                loadmob(mob);
+        })
     });
 }
 
@@ -96,29 +75,7 @@ function sel2()
         Array.from(SelElem.selectedOptions).every(dim => {
             if (mob.dimension.includes(dim.value))
             {
-                const div = document.createElement("div");
-                div.className = "mob";
-                const img = document.createElement("img");
-                img.src = mob.image;
-                img.alt = mob.name;
-                img.id = "creature";
-                div.appendChild(img);
-                const h1 = document.createElement("h1");
-                h1.innerHTML=mob.name;
-                div.appendChild(h1);
-                const ddiv = document.createElement("div");
-                ddiv.style.width="45%";
-                ddiv.style.float="right";
-                ddiv.style.margin=0;
-                ddiv.className="general";
-                ddiv.innerHTML="HP: <br>";
-                for (i=1; i<=20;i++)
-                    i%2==1 ? ddiv.innerHTML+='<img src="képek/heart.png" class="heart">' :  ddiv.innerHTML+='<img src="képek/heart.png" class="heart flipped">' ;
-                ddiv.innerHTML+="<br>Atk: <br>";
-                for (i=1; i<=1;i++)
-                    i%2==1 ? ddiv.innerHTML+='<img src="képek/heart.png" class="heart">' :  ddiv.innerHTML+='<img src="képek/heart.png" class="heart flipped">' ;
-                div.appendChild(ddiv);
-                document.getElementById("mobs").appendChild(div);
+                loadmob(mob)
                 return false;
             }
             else
@@ -139,32 +96,37 @@ function sel3()
     mobs.forEach(mob => {
         if (mob.name.toLowerCase().includes(Searched.value.toLowerCase()))
         {
-                    const div = document.createElement("div");
-                    div.className = "mob";
-                    const img = document.createElement("img");
-                    img.src = mob.image;
-                    img.alt = mob.name;
-                    img.id = "creature";
-                    div.appendChild(img);
-                    const h1 = document.createElement("h1");
-                    h1.innerHTML=mob.name;
-                    div.appendChild(h1);
-                    const ddiv = document.createElement("div");
-                    ddiv.style.width="45%";
-                    ddiv.style.float="right";
-                    ddiv.style.margin=0;
-                    ddiv.className="general";
-                    ddiv.innerHTML="HP: <br>";
-                    for (i=1; i<=20;i++)
-                        i%2==1 ? ddiv.innerHTML+='<img src="képek/heart.png" class="heart">' :  ddiv.innerHTML+='<img src="képek/heart.png" class="heart flipped">' ;
-                    ddiv.innerHTML+="<br>Atk: <br>";
-                    for (i=1; i<=1;i++)
-                        i%2==1 ? ddiv.innerHTML+='<img src="képek/heart.png" class="heart">' :  ddiv.innerHTML+='<img src="képek/heart.png" class="heart flipped">' ;
-                    div.appendChild(ddiv);
-                    document.getElementById("mobs").appendChild(div);
-                
+                    
+                loadmob(mob);
         }
     });
+}
+
+function loadmob(mob)
+{
+    const div = document.createElement("div");
+    div.className = "mob";
+    const img = document.createElement("img");
+    img.src = mob.image;
+    img.alt = mob.name;
+    img.id = "creature";
+    div.appendChild(img);
+    const h1 = document.createElement("h1");
+    h1.innerHTML=mob.name;
+    div.appendChild(h1);
+    const ddiv = document.createElement("div");
+    ddiv.style.width="45%";
+    ddiv.style.float="right";
+    ddiv.style.margin=0;
+    ddiv.className="general";
+    ddiv.innerHTML="HP: <br>";
+    for (i=1; i<=mob.health;i++)
+        i%2==1 ? ddiv.innerHTML+='<img src="képek/heart.png" class="heart">' :  ddiv.innerHTML+='<img src="képek/heart.png" class="heart flipped">' ;
+    ddiv.innerHTML+="<br>Atk: <br>";
+    for (i=1; i<=mob.dmg;i++)
+        i%2==1 ? ddiv.innerHTML+='<img src="képek/heart.png" class="heart">' :  ddiv.innerHTML+='<img src="képek/heart.png" class="heart flipped">' ;
+    div.appendChild(ddiv);
+    document.getElementById("mobs").appendChild(div);
 }
 
 function cave(lgt)
@@ -246,7 +208,25 @@ function cave(lgt)
         }
         lol.appendChild(tavolo);
 }
-
+function funfact()
+{
+    const fact = [
+        "<a href='https://www.youtube.com/shorts/RlEt4Pa6IWA' target='_blank'>Creepers were made by accident.</a> Just like you!",
+        "It takes 1,031 diamonds to craft a full inventory of diamond blocks. That’s more diamonds than you’ll ever find—or afford!",
+        "<a href= 'https://www.youtube.com/shorts/Hk6pXoywwmA' target='_blank'>Endermen speak reversed English phrases.</a> Kind of like how no one understands you half the time",
+        "<a href='https://www.youtube.com/shorts/APR-OMq1Vrw' target='_blank'>Ghasts make their sounds from a cat’s</a> sleeping noises. Which is ironic, because unlike you, cats know how to chill.",
+        "The End was originally called 'The Sky Dimension.' Much like your goals: all talk, no reality.",
+        "Sheep regrow wool by eating grass. That’s right; even sheep know how to handle self-care better than you.",
+        "The Wither drops a Nether Star. Too bad no one dropped a star when you were born.",
+        "Baby zombies are faster than regular zombies. A bit like toddlers—minus the excuses for being so small and annoying.",
+        "The Ender Dragon is the only boss mob in the base game. Unlike you, it actually has a purpose.",
+        "<a href='https://scaleofuniverse.com/en/universe/minecraft-world' target='_blank'>Minecraft’s map is 60 million blocks wide.</a> Which is about as wide as your search history of failed ideas.",
+        "Piglins love gold items and will ignore you if you're holding one. But don’t get your hopes up; no amount of gold will make people ignore your awkwardness.",
+        "You can ride pigs, but they won’t take you far. Kind of like your dreams.",
+        "Minecraft mobs might be dangerous, but at least they don’t have Wi-Fi to roast you for your poor gameplay."
+    ]
+    document.getElementById("funfact").innerHTML=`<h2>Fun fact:</h2><p>${fact[parseInt(Math.random()*13)]}</p>`;
+}
 function mob (name, health, dmg, type, dimension)
 {
     this.name = name;
